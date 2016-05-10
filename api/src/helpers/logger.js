98a64680b103cee242583e4db2691cb5101fdac7
Shreyansh Pandey <334@doonschool.com>
2016-05-11 00:26:52 +0530

    added Winston for logging; configured logging

---
+++ api/src/helpers/logger.js
@@ -0,0 +1,32 @@
+const Winston = require( 'winston' );
+
+Winston.add( require( 'winston-graylog2' ), {
+
+    name: 'DSMUN - Primary EC2',
+    level: 'error',
+    graylog: {
+
+        servers: [
+
+            {
+                host: '52.76.9.136',
+                port: '12201'
+            }
+
+        ],
+
+        facility: 'DSMUN_MANAGEMENT_APPLICATION'
+
+    }
+
+} );
+
+Winston.add( Winston.transports.Console, {
+
+    name: 'Console',
+    colorize: true,
+    timestamp: true,
+
+} );
+
+export default Winston;
