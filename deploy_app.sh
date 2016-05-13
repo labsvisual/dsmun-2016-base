5110bf0e68de8bf2ea620800096f0fe2051527e8
Shreyansh Pandey <334@doonschool.com>
2016-05-13 10:37:51 +0530

    DEPLOY_PREP

---
+++ deploy_app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git add .
+git commit -am "DEPLOY_PREP"
+git push -u origin develop
+
+cd api/
+echo "Deploying on dsmun-backend"
+npm run deploy
+
+cd ../website
+echo "Deploying dsmun-website"
+npm run deploy
+
+cd ../
+echo "Deploying dsmun-frontend"
+npm run deploy
+
+echo "\r\n"
+echo "ALL SYSTEMS DEPLOYED"
