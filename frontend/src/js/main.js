bc7cf12563d693e705b335cd4c88bdbb33507404
Shreyansh Pandey <334@doonschool.com>
2016-05-26 11:57:04 +0530

    minor logging changes

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,11 +1,5 @@
 $( document ).ready( function() {
 
-    // if ( $('.datepicker').type != 'date' ) {
-    //
-    //     $( '.datepicker' ).datepicker();
-    //
-    // }
-
     $( '#header' ).click( function() {
 
         $('.ui.sidebar').sidebar('toggle');
