408f655871d6571bac9574adaa188ee9b7e04d93
Shreyansh Pandey <334@doonschool.com>
2016-07-29 10:01:58 +0530

    removed graylog2

--- api/src/helpers/logger.js
+++ api/src/helpers/logger.js
@@ -3,29 +3,6 @@ const Winston = require( 'winston' )
     , _       = require( 'lodash' )
     , RandomString = require( 'randomstring' );
 
-if( !debug ) {
-    Winston.add( require( 'winston-graylog2' ), {
-
-        name: 'DSMUN - Management Application',
-        level: 'error',
-        graylog: {
-
-            servers: [
-
-                {
-                    host: '52.77.145.233',
-                    port: '5555'
-                }
-
-            ],
-
-            facility: 'DSMUN_MANAGEMENT_APPLICATION'
-
-        }
-
-    } );
-}
-
 Winston.add( Winston.transports.Console, {
 
     name: 'Console',
